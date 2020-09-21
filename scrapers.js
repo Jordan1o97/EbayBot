72af7f9b54630c1cea3f875ccf9bb6d135a20de9
Jordan Davis <jordan@Jordans-MacBook-Pro.local>
2020-09-21 00:21:36 -0300

    Zach's Butthole 1

--- scrapers.js
+++ scrapers.js
@@ -1,1 +1,17 @@
 const puppeteer = require('puppeteer');
+
+async function scrapeProduct(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const [el] = await page.$x('/html/body/div[6]/div[1]/div/table/tbody/tr/td/div/img');
+    const src = await el.getProperty('src');
+    const srcTxt = await src.jsonValue();
+
+    console.log({srcTxt});
+    browser.close()
+}
+
+scrapeProduct('https://www.ebay.ca/itm/Real-32GB-64GB-128GB-Cartoon-Animals-USB-2-0-Thumb-Memory-Stick-Flash-Drive-Gift/401620380208?hash=item5d8270aa30:g:fqcAAOSwd2ZbytsC&var=671364717418%27);
